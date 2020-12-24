85b8ef91fac6c57b79df455577311d18c1e2a659
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-23 21:50:57 -0800

    feat: create frontend setup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from '../src/Link'
+
+export default function Index() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Link href="/about">Go to the about page</Link>
+    </>
+  )
+}
