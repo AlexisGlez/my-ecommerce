5d17653d394358790c055f20930a99e777b69974
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-25 18:53:19 -0800

    feat: create app header (#3)

--- frontend/src/App/theme.ts
+++ frontend/src/App/theme.ts
@@ -1,3 +1,8 @@
-import { extendTheme } from '@chakra-ui/react'
+import { theme as chakraTheme, extendTheme } from '@chakra-ui/react'
 
-export const theme = extendTheme({})
+export const theme = extendTheme({
+  fonts: {
+    body: `Nunito Sans,${chakraTheme.fonts.body}`,
+    heading: `Nunito Sans,${chakraTheme.fonts.heading}`,
+  },
+})
