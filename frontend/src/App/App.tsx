863c97a7d39b63f1044fe52c74afdd1825764ab9
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2021-01-25 22:36:22 -0800

    feat: create cookies class to persist data in client side

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,5 +1,6 @@
 /// <reference path="./App.d.ts" />
 
+import { useEffect } from 'react'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, Box } from '@chakra-ui/react'
@@ -7,8 +8,18 @@ import { ChakraProvider, Box } from '@chakra-ui/react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { theme } from './theme'
+import { Cookies } from '@app-shared/Cookies'
+import { CartStore } from '@app-stores/CartStore'
 
 export function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const cookies = Cookies.getAll()
+
+    if (cookies[Cookies.Cart]) {
+      CartStore.initialize(cookies[Cookies.Cart])
+    }
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <Head>
---
+++ frontend/src/shared/Cookies/Cookies.ts
@@ -0,0 +1,44 @@
+import jsHttpCookie from 'cookie'
+import JSCookies from 'js-cookie'
+
+export class Cookies {
+  public static readonly Cart = 'cart'
+
+  public static set(name: string, value: any) {
+    if (value != null) {
+      JSCookies.set(name, value, { expires: Infinity })
+    }
+  }
+
+  public static getString(name: string) {
+    return JSCookies.get(name)
+  }
+
+  public static getNumber(name: string) {
+    const value = JSCookies.get(name)
+    if (value && typeof value === 'string') {
+      return Number(value)
+    }
+    return undefined
+  }
+
+  public static getObject(name: string) {
+    return JSCookies.getJSON(name)
+  }
+
+  public static getAll() {
+    return JSCookies.getJSON()
+  }
+
+  public static remove(name: string) {
+    JSCookies.remove(name)
+  }
+
+  public static parseHttpCookies(cookies: string) {
+    if (cookies && typeof cookies === 'string') {
+      return jsHttpCookie.parse(cookies)
+    }
+
+    return null
+  }
+}
