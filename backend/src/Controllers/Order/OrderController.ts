71d45a4713da3ae065843ab6942c77b2a2f94cfb
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2021-02-26 20:34:21 -0800

    feat: create order controller and router (#39)

---
+++ backend/src/Controllers/Order/OrderController.ts
@@ -0,0 +1,46 @@
+import { Response } from 'express'
+
+import { OrderModel } from '@app-models/Order'
+import { RequestWithUser } from '@app-middlewares/auth'
+
+export class OrderController {
+  public static async postOrder(req: RequestWithUser, res: Response) {
+    if (!req.user?._id) {
+      res.status(401).json({ data: null, message: 'Unauthorized. Invalid user.' })
+      return
+    }
+
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body
+
+    if (!orderItems || !orderItems.length) {
+      res.status(400).json({ data: null, message: 'No order items.' })
+      return
+    }
+
+    try {
+      const order = new OrderModel({
+        user: req.user._id,
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      })
+
+      const createdOrder = await order.save()
+
+      res.status(201).json({ data: createdOrder, message: 'Order created.' })
+    } catch (error) {
+      console.error('An error happened while creating the order:', error)
+      res.status(500).json({ data: [], message: 'Unable to create order.' })
+    }
+  }
+}
