2019c898d0b18e72a90257b8ab8c8f37817252b1
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2021-03-04 19:33:11 -0800

    feat: create all user orders endpoint

--- backend/src/Routes/Order/OrderRouter.ts
+++ backend/src/Routes/Order/OrderRouter.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from '@app-middlewares/auth'
 const OrderRouter = express.Router()
 
 OrderRouter.route('/').post(authMiddleware, OrderController.postOrder)
+OrderRouter.route('/all').get(authMiddleware, OrderController.getAllUserOrders)
 OrderRouter.route('/:id').get(authMiddleware, OrderController.getOrderById)
 OrderRouter.route('/:id/pay').patch(authMiddleware, OrderController.payOrder)
 
