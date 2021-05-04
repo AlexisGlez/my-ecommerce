17db9b2aec5a9884c693d73f94d846d37e2d3087
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2021-05-03 17:12:04 -0700

    chore: add babel

--- backend/build/Middlewares/urlNotFound/urlNotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.urlNotFound = void 0;
-const urlNotFound = (req, res, next) => {
-    const error = new Error(`URL not found: ${req.originalUrl}`);
-    res.status(404).json({ message: 'URL not found.' });
-    next(error);
-};
-exports.urlNotFound = urlNotFound;
