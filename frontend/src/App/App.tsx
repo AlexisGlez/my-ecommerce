045836fd3093ae38f6f40c24d7632961ed52c906
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2021-06-01 17:17:08 -0700

    feat: add manifest and icons to html

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -33,6 +33,10 @@ export function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
